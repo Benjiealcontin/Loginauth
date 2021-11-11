66de78492a0e133a788aaef65879805ad899b1d8
Benjiealcontin <benjiealcontin23@gmail.com>
2021-11-11 14:32:11 -0800

    Login success

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,34 +1,56 @@
 const User = require("../models/User");
+const ErrorResponse = require ('../utils/errorResponse')
 
 
 // @desc    Register user
 exports.register = async (req, res, next) => {
     const { firstname, lastname, email, password } = req.body;
   
-    try {
+    try {  
       const user = await User.create({
         firstname,
-        lastname,
-        email,
+        lastname,                                
+        email,                                      
         password,
       });
 
-      res.status(201).json({
-          success: true,
-          user,
-      });
+      sendtoken(user, 201, res);
+
     }catch (error){
-        res.status(500).json({
-            success: false,
-            error: error.message,
-        });
+        next(error);
     }
 
 };
 
-exports.login = (req, res, next) => {
-    res.send('Login route');
-};
+// @desc    Login user
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    // Check if email and password is provided
+    if (!email || !password) {
+      return next(new ErrorResponse("Please provide an email and password", 400));
+    }
+  
+    try {
+      // Check that user exists by email
+      const user = await User.findOne({ email }).select("+password");
+  
+      if (!user) {
+        return next(new ErrorResponse("Invalid credentials", 401));
+      }
+  
+      // Check that password match
+      const isMatch = await user.matchPasswords(password);
+  
+      if (!isMatch) {
+        return next(new ErrorResponse("Invalid credentials", 401));
+      }
+  
+      sendToken(user, 200, res);
+    } catch (err) {
+      next(err);
+    }
+  };
 
 exports.forgotpassword = (req, res, next) => {
     res.send('Forgot password route');
@@ -37,3 +59,8 @@ exports.forgotpassword = (req, res, next) => {
 exports.resetpassword = (req, res, next) => {
     res.send('Reset password route');
 };
+
+const sendToken = (user, statusCode, res) =>{
+    const token = user.getSignedToken();
+    res.status(statusCode).json({success: true, token})
+}
